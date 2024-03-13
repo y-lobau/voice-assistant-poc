ce92b593fd6251119dcc56a7912345c8df7e765f
Yan Lobau <y_lobov@flo.health>
2024-03-13 19:42:39 +0200

    Added blinking to other actions

--- src/infrastructure/visualisation/DeviceVisualization.ts
+++ src/infrastructure/visualisation/DeviceVisualization.ts
@@ -8,22 +8,26 @@ export class DeviceVisualization implements IVisualFeedback {
 
   initializing(start: boolean = true) {
     this.console.info("initializing...");
+    this.fireLed(start);
   }
   listening(start: boolean = true) {
     this.console.info("listening...");
-    if (start) {
-      this.LED.writeSync(1);
-    } else {
-      this.LED.writeSync(0);
-    }
+    this.fireLed(start);
   }
   talking(start: boolean = true) {
     this.console.info("talking...");
+    this.fireLed(start);
   }
   thinking(start: boolean = true) {
     this.console.info("thinking...");
+    this.fireLed(start);
   }
   waiting(start: boolean = true) {
     this.console.info("waiting...");
+    this.fireLed(start);
+  }
+
+  fireLed(on: boolean) {
+    this.LED.writeSync(on ? 1 : 0);
   }
 }
