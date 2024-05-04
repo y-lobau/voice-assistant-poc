8de873abb28adbf657b3cd7ec780b3dd42e97192
Yan Lobau <y_lobov@flo.health>
2024-05-04 10:11:03 +0300

    rewritten

--- test-led.js
+++ test-led.js
@@ -1,52 +1,89 @@
 import spi from 'spi-device';
 
-// Open SPI bus
-const ledStrip = spi.open(0, 1, err => {
-  if (err) throw err;
-});
-
-// Function to send data to the LED strip
-const sendLedData = (data) => {
-  const message = [{
-    sendBuffer: Buffer.from(data),
-    byteLength: data.length,
-    speedHz: 2000000 // SPI speed
-  }];
-
-  ledStrip.transfer(message, (err) => {
-    if (err) throw err;
-  });
-};
-
-// Function to turn on LEDs in yellow color
-const turnOnLEDs = () => {
-  const startFrame = [0x00, 0x00, 0x00, 0x00];
-  const endFrame = [0xFF, 0xFF, 0xFF, 0xFF];
-  const ledFrames = [];
-  const yellow = [0xFF, 0xFF, 0x00]; // GRB format for yellow
-
-  for (let i = 0; i < 3; i++) {
-    ledFrames.push(0xE0 + 31, ...yellow); // Full brightness
+const RGB_MAP = {
+    'rgb': [3, 2, 1],
+    'rbg': [3, 1, 2],
+    'grb': [2, 3, 1],
+    'gbr': [2, 1, 3],
+    'brg': [1, 3, 2],
+    'bgr': [1, 2, 3]
+  };
+  
+  class APA102 {
+    constructor(numLed, globalBrightness = 31, order = 'rgb', bus = 0, device = 1, maxSpeedHz = 8000000) {
+      this.numLed = numLed;
+      this.order = order.toLowerCase();
+      this.rgb = RGB_MAP[this.order] || RGB_MAP['rgb'];
+      this.globalBrightness = Math.min(globalBrightness, 31);
+      this.leds = new Array(this.numLed * 4).fill(0);
+      this.spi = spi.open(bus, device, {
+        mode: spi.MODE0,
+        maxSpeedHz: maxSpeedHz
+      });
+    }
+  
+    sendFrame(bytes) {
+      return new Promise((resolve, reject) => {
+        const message = [{
+          sendBuffer: Buffer.from(bytes),
+          byteLength: bytes.length,
+          speedHz: this.spi.maxSpeedHz
+        }];
+        this.spi.transfer(message, (err, message) => {
+          if (err) reject(err);
+          else resolve();
+        });
+      });
+    }
+  
+    async clockStartFrame() {
+      await this.sendFrame([0, 0, 0, 0]); // Start frame
+    }
+  
+    async clockEndFrame() {
+      await this.sendFrame(new Array((this.numLed / 2 + 1) & ~0x03).fill(0xFF));
+    }
+  
+    async show() {
+      await this.clockStartFrame();
+      await this.sendFrame(this.leds);
+      await this.clockEndFrame();
+    }
+  
+    setPixel(ledNum, red, green, blue, brightPercent = 100) {
+      if (ledNum < 0 || ledNum >= this.numLed) return;
+      const brightness = Math.ceil(brightPercent * this.globalBrightness / 100.0);
+      const ledStart = (brightness & 0x1F) | 0xE0;
+      const index = 4 * ledNum;
+      this.leds[index] = ledStart;
+      this.leds[index + this.rgb[0]] = red;
+      this.leds[index + this.rgb[1]] = green;
+      this.leds[index + this.rgb[2]] = blue;
+    }
+  
+    cleanup() {
+      this.spi.close();
+    }
+  
+    static combineColor(red, green, blue) {
+      return (red << 16) | (green << 8) | blue;
+    }
+  
+    wheel(wheelPos) {
+      if (wheelPos > 255) wheelPos = 255;
+      if (wheelPos < 85) {
+        return APA102.combineColor(wheelPos * 3, 255 - wheelPos * 3, 0);
+      } else if (wheelPos < 170) {
+        wheelPos -= 85;
+        return APA102.combineColor(255 - wheelPos * 3, 0, wheelPos * 3);
+      } else {
+        wheelPos -= 170;
+        return APA102.combineColor(0, wheelPos * 3, 255 - wheelPos * 3);
+      }
+    }
   }
+  
+  module.exports = APA102;
 
-  const data = [].concat(startFrame, ledFrames, endFrame);
-  sendLedData(data);
-};
-
-// Function to turn off LEDs
-const turnOffLEDs = () => {
-  const startFrame = [0x00, 0x00, 0x00, 0x00];
-  const endFrame = [0xFF, 0xFF, 0xFF, 0xFF];
-  const ledFrames = [];
-
-  for (let i = 0; i < 3; i++) {
-    ledFrames.push(0xE0, 0x00, 0x00, 0x00); // Off (zero brightness)
-  }
-
-  const data = [].concat(startFrame, ledFrames, endFrame);
-  sendLedData(data);
-};
-
-// Example usage
-turnOnLEDs(); // This will turn on the LEDs in yellow
-setTimeout(turnOffLEDs, 5000); // This will turn off the LEDs after 2 seconds
+  const apa = new APA102(3);
+  apa.setPixel(0, 255, 0, 0);
