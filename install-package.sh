f6b639c4ed301917c7d31ce078ea4c0575a637a5
Yan Lobau <y_lobov@flo.health>
2024-07-11 06:53:47 +0300

    Added local speech-recorder for arm64

---
+++ install-package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  echo "installing speech-recorder on mac..."
+  npm install speech-recorder
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux (assuming this is your Raspberry Pi)
+  echo "installing speech-recorder on rpi..."
+  cd /path/to/your/local/speech-recorder
+  npm link
+  cd /path/to/your/project
+  npm link speech-recorder
+else
+  echo "Unsupported OS: $OSTYPE"
+  exit 1
+fi
